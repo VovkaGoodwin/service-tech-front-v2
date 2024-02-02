de1e0445287d87d6e98cc9ef5eb933e71d7fba9d
Vladimir Radko <volodya.radko@gmail.com>
2024-02-02 13:55:19 +0700

    chore: конфиг прокси

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,31 @@
-import { defineConfig } from 'vite';
+import { defineConfig, UserConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(import.meta.dir, './src'),
-      shared: resolve(import.meta.dir, './src/shared'),
-      entities: resolve(import.meta.dir, './src/entities'),
-      features: resolve(import.meta.dir, './src/features'),
-      widgets: resolve(import.meta.dir, './src/widgets'),
-      pages: resolve(import.meta.dir, './src/pages'),
+type Proxy = UserConfig['server']['proxy']
+
+export default defineConfig(({ mode }) => {
+
+  const proxy: Proxy = mode === 'development' ? {
+    '/api': {
+      target: 'http://localhost:8000/api'
     }
-  }
+  } : {};
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy
+    },
+    resolve: {
+      alias: {
+        '@': resolve(import.meta.dir, './src'),
+        shared: resolve(import.meta.dir, './src/shared'),
+        entities: resolve(import.meta.dir, './src/entities'),
+        features: resolve(import.meta.dir, './src/features'),
+        widgets: resolve(import.meta.dir, './src/widgets'),
+        pages: resolve(import.meta.dir, './src/pages'),
+      }
+    }
+  };
 });
